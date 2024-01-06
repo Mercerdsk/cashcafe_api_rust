f200f814b779e144ae422afeb472f64011de5487
sasikumar <sasikumar.d@gitechgames.com>
2024-01-06 12:17:51 +0530

    version date change

--- src/api/endpoints.rs
+++ src/api/endpoints.rs
@@ -19,7 +19,7 @@ use reqwest::Client;
 #[get("/get_version/")]
 async fn get_version_handler(req:HttpRequest)-> Result<impl Responder,Box<dyn std::error::Error>>{
     
-    let parsed: Value = serde_json::from_str("{\"result\":{\"Date\":\"2024-01-05\",\"Version\":\"Version : 1.0.8\"}}")?;
+    let parsed: Value = serde_json::from_str("{\"result\":{\"Date\":\"2024-01-06\",\"Version\":\"Version : 1.0.9\"}}")?;
     return Ok(web::Json(parsed)) 
     
 }
